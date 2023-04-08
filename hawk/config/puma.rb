6ead48a31d0f02ba7e6766d72344b0294a009af3
Aleksei Burlakov <aburlakov@suse.com>
2023-04-08 16:15:37 +0200

    It starts under the new rails, but doesn't load the page
    It starts as both
    hawk/config> puma -C puma.rb
    and
    hawk> rails server

--- hawk/config/puma.rb
+++ hawk/config/puma.rb
@@ -11,12 +11,12 @@ environment @environment
 
 tag "hawk"
 
-daemonize false
+# daemonize false
 prune_bundler false
 
-threads 0, 1
+# threads 0, 1
 
-workers 1
+# workers 1
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
@@ -24,7 +24,7 @@ workers 1
 # process behavior so workers use less memory.
 preload_app!
 
-worker_timeout 60
+# worker_timeout 60
 
 pidfile File.join(ROOT, "tmp", "pids", "puma.pid")
 state_path File.join(ROOT, "tmp", "pids", "puma.state")
