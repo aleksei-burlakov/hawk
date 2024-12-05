1a95655a2e32b50cb847def694d7f5a886812c21
Aleksei Burlakov <aburlakov@suse.com>
2024-12-05 04:44:30 +0100

    Disable regex in the assets
    (to make it compatible with sprockets4+sass-rails6)

--- hawk/config/initializers/assets.rb
+++ hawk/config/initializers/assets.rb
@@ -5,14 +5,15 @@ Rails.application.config.tap do |config|
   config.assets.version = "1.0"
 
   config.assets.precompile = [
-    /locale\/.+\.(css|js)$/,
-    /gettext\.(css|js)$/,
-    /application\.(css|js)$/,
-    /authentication\.(css|js)$/,
-    /dashboard.(css|js)$/,
-    /ie\.(css|js)$/,
-    /vendor\.(css|js)$/,
-    /\.(jpg|png|gif|svg|ico|eot|woff|woff2|ttf)$/
+    "locale*.css", "locale*.js",
+    "gettext.css", "gettext.js",
+    "application.css","application.js",
+    "authentication.css", "authentication.js",
+    "dashboard.css", "dashboard.js",
+    "ie.css", "ie.js",
+    "vendor.css", "vendor.js",
+    "*.jpg", "*.png", "*.gif", "*.svg",
+    "*.ico", "*.eot", "*.woff", "*.woff2", "*.ttf"
   ]
 
   config.assets.paths << config.root.join(
